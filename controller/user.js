781b06c868389d156522a3d09edfc3ce3e0e5be1
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-05-23 22:26:56 +0100

    JWT Authorization

--- controller/user.js
+++ controller/user.js
@@ -1,12 +1,36 @@
 //JWT
 const jwt = require('jsonwebtoken')
+const secretWord = 'SAKARIOUS'
 // USERS CONTROLLER
 const userServices = require('../services/users')
 
 module.exports = class userController {
     static loginRequired(req, res, next){
-        if (req.user){
-            next()
+        if (req.headers && req.headers.authorization){
+            let splitedAuth = req.headers.authorization.split(' ')[0]
+            let token = req.headers.authorization.split(' ')[1]
+            if (splitedAuth !== 'Bearer'){
+                res.status(400).json({message: "Authorization format is Bearer <token>"})
+            } else {
+                jwt.verify(token, secretWord, (err, decode) => {
+                    if (err){
+                        req.user = undefined
+                        res.status(500).json({message: `Authorization Failed. ${err.message}` })
+                        return
+                    }
+
+                    if(!decode){
+                        req.user = undefined
+                        res.status(401).json({message: `Invalid Authorization Token. Please try again.` })
+                        return
+                    } else {
+                        req.user = decode
+                        next()
+                    }
+                })
+
+
+            }
         } else {
             res.status(400).json({message: "Authorization Failed. User not authenticated"})
             return
@@ -27,7 +51,7 @@ module.exports = class userController {
             firstname: regDetails.firstname,
             lastname: regDetails.lastname,
             username: regDetails.username
-        }, 'SAKARIOUS', 
+        }, secretWord, 
         { expiresIn: '1h' });
 
         res.json({message: 'Registration Successful', username: regDetails.username, token, error: null})
@@ -49,7 +73,7 @@ module.exports = class userController {
             firstname: loginDetails.firstname,
             lastname: loginDetails.lastname,
             username: loginDetails.username
-        }, 'SAKARIOUS', 
+        }, secretWord, 
         { expiresIn: '1h' });
 
         res.json({message: 'Login Successful', username: loginDetails.username, token, error: null})
